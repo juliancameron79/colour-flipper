411fc2a29e0b8fed48eecdeb0d2347ea6aa5b2fb
juliancameron79 <juliancameron79@gmail.com>
2021-01-15 14:06:15 +0100

    Added event listener along with background colour and text content function

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colours = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const btn = document.getElementById('btn');
+const colour = document.querySelector('.colour');
+
+btn.addEventListener('click', function () {
+  // get random number between 0 - 3
+  const randomNumber = 2;
+  document.body.style.backgroundColor = colours[randomNumber];
+  colour.textContent = colours[randomNumber];
+});
