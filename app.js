cf079a37ba56406d61348019a2da4ad69aecb784
juliancameron79 <juliancameron79@gmail.com>
2021-01-15 14:17:51 +0100

    Added random number function

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const colour = document.querySelector('.colour');
 
 btn.addEventListener('click', function () {
   // get random number between 0 - 3
-  const randomNumber = 2;
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
   document.body.style.backgroundColor = colours[randomNumber];
   colour.textContent = colours[randomNumber];
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colours.length);
+}
